2fb88eada6f082996c38252e778b556ae1200bcb
Kshitij Sachdeva <k.sachdeva0704@gmail.com>
2017-03-25 23:32:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ function createtemplate(data){
         </head>
         <body>
           <div class = "container">
-          <div> <a href = "/ui/appindex.html" >Home</a></div>
+          <div> <a href = "/webapp" >Home</a></div>
           <hr/>
           <div>
               <h3>${heading}</h3>
