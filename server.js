0fa4566ea0d51e802d67c990446e8fcfab2e1ea1
Kshitij Sachdeva <k.sachdeva0704@gmail.com>
2017-03-25 23:23:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,6 +113,9 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/ui/960x360-cover.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '960x360-cover.png'));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
